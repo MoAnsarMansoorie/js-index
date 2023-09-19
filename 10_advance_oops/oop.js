c9bce8e87ebd79e4b4fbfad300c8cd6ad8d1dfdc
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2023-09-19 22:42:39 +0530

    oops

--- 10_advance_oops/oop.js
+++ 10_advance_oops/oop.js
@@ -35,3 +35,12 @@ const userOne = new User("hitesh", 12, true)
 const userTwo = new User("ChaiAurCode", 11, false)
 console.log(userOne.constructor);
 //console.log(userTwo);
+
+
+// new
+// step-1: whenever you use new keyword, then an empty object eill be created. jisko instance bola jata hai
+// step-2: new keyword ke karan constructor function call hota hai jo sare argument ko pack karts hai aur humko de deta hai
+// step:3: jo bhi argument lihhe hote hai wo this keyword se bane variable me store ho jate hai
+// step-4: sare argument mil jate hai function ke andar
+
+// read - instanceOf
